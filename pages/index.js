6d21dc3eaaed182fa3b7da6c17f1c67d215985a8
Joshua Melo <joshuamelo@gmail.com>
2021-01-26 01:28:45 -0500

    added metadata

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ export default function Home() {
       <Head>
         <title>Joshua Melo - Portfolio</title>
         <link rel="icon" href="/static/favicon.ico" />
+        <meta property="og:title" content="Joshua Melo - Portfolio" />
+        <meta property="og:image" content="/images/pageImage.PNG" />
+        <meta property="og:url" content="https://joshuamelo.vercel.app/" />
+        <meta property="og:description" content="Joshua Melo's Perfolio site" />
       </Head>
       <ProjectsContainer />
     </>
